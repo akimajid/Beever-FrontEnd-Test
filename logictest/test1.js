148727a2e8839978aa456a70a0e9f3a1d0afaab9
akimajid <akimajid07@gmail.com>
2022-08-11 12:59:24 +0700

    test1 answer

--- logictest/test1.js
+++ logictest/test1.js
@@ -1,4 +1,10 @@
 let words = "beever";
+let result = []
+
+for (let i = 0; i <= words.length; i++) {
+    result.push(words[i])
+    console.log(result)
+}
 
 // Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut : 
 // b
